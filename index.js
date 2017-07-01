84cdfc5a90ebd5a692c6c457eec5ea53a2a88143
dwqs <461147874@qq.com>
2017-07-01 12:18:00 +0800

    [feature]add entry

---
+++ index.js
@@ -0,0 +1,7 @@
+/**
+ * Created by pomy on 01/07/2017.
+ */
+
+import js2excel from './src/js2excel';
+
+export default js2excel;
---
+++ src/js2excel.js
@@ -0,0 +1,5 @@
+/**
+ * Created by pomy on 01/07/2017.
+ */
+
+export default js2excel;
