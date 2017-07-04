232f797dc1ec8e49cbee29e634fb302da4941f42
dwqs <461147874@qq.com>
2017-07-05 00:18:12 +0800

    [misc]add rollup config

---
+++ rollup.config.js
@@ -0,0 +1,36 @@
+/**
+ * Created by pomy on 03/07/2017.
+ */
+
+import commonjs from 'rollup-plugin-commonjs';
+import resolve from 'rollup-plugin-node-resolve';
+import typescript from 'rollup-plugin-typescript';
+import babel from 'rollup-plugin-babel';
+import uglify from 'rollup-plugin-uglify';
+
+export default {
+    entry: 'src/test.ts',
+    format: 'umd',
+    moduleName: 'test',
+    dest: 'dist/test.min.js',
+    plugins: [
+        resolve({
+            customResolveOptions: 'node_modules',
+            jsnext: true
+        }),
+        commonjs(),
+        typescript(),
+        babel({
+            exclude: 'node_modules/**',
+            externalHelpers: true
+        }),
+        uglify({
+            compress: {
+                warnings: false,
+                drop_debugger: true,
+                drop_console: true
+            },
+            mangle: true
+        })
+    ]
+}
