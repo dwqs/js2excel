a895710858b98e80f10288182f4890394a2119e9
dwqs <461147874@qq.com>
2017-07-05 20:28:08 +0800

    [misc]with buble replace babel

--- rollup.config.js
+++ rollup.config.js
@@ -5,7 +5,7 @@
 import commonjs from 'rollup-plugin-commonjs';
 import resolve from 'rollup-plugin-node-resolve';
 import typescript from 'rollup-plugin-typescript';
-import babel from 'rollup-plugin-babel';
+import buble from 'rollup-plugin-buble';
 import builtins from 'rollup-plugin-node-builtins';
 
 export default {
@@ -21,9 +21,6 @@ export default {
         }),
         commonjs(),
         typescript(),
-        babel({
-            exclude: 'node_modules/**',
-            externalHelpers: true
-        })
+        buble()
     ]
 }
