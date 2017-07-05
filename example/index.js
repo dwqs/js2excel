8edbd78c0cc78bcb9d5fa8728a95bc35c3b54faf
dwqs <461147874@qq.com>
2017-07-05 10:17:41 +0800

    [misc]add example

---
+++ example/index.js
@@ -0,0 +1,47 @@
+'use strict';
+
+import js2excel from '../src/js2excel';
+
+let btn = document.getElementById('export');
+
+let columns = [
+    {
+        name: 'User Id',
+        prop: 'userId'
+    },
+    {
+        name: 'Phone Number',
+        prop: 'userPhoneNumber'
+    },
+    {
+        name: 'User Address',
+        prop: 'userAddress'
+    }
+];
+
+// rows' data will be exports, which you probably get it from server.
+let rows = [
+    {
+        "userId": 1,
+        "userPhoneNumber": 1888888888,
+        "userAddress": 'xxxx'
+    },
+    {
+        "userId": 2,
+        "userPhoneNumber": 1888888888,
+        "userAddress": 'xxxx'
+    },
+    {
+        "userId": 3,
+        "userPhoneNumber": 1888888888,
+        "userAddress": 'xxxx'
+    }
+];
+
+btn.addEventListener('click', () => {
+    try{
+        js2excel(columns, rows, 'user-info-data');
+    } catch (e) {
+        console.error('export error', e.message);
+    }
+}, false);
