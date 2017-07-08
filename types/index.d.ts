9e99ec0ba038a0f13c7599d51920c7a9669c3031
dwqs <461147874@qq.com>
2017-07-08 22:11:16 +0800

    [misc]v0.7.5

---
+++ types/index.d.ts
@@ -0,0 +1,12 @@
+export interface Params {
+    headers: any[];
+    rows: any[];
+    name?: string;
+    formateDate?: string;
+}
+export declare type CallBack = (data: any) => any;
+declare const js2excel: {
+    excel2json: (files: File[], cb: CallBack, defval?: string) => any;
+    json2excel: (opts: Params) => void;
+};
+export default js2excel;
