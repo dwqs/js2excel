c11bb0ffeb2972f7382348b226052abed4ef703e
dwqs <461147874@qq.com>
2018-08-03 10:48:24 +0800

    v1.0.1

--- types/index.d.ts
+++ types/index.d.ts
@@ -4,9 +4,12 @@ export interface Params {
     formateDate?: string;
 }
 export declare type CallBack = (data: any) => any;
+declare function json2excel(opts: Params): void;
+declare function excel2json(files: File[], cb: CallBack, defval?: string): any;
+declare function csv2json(files: File[], cb: CallBack, encode?: string, separator?: string): any;
 declare const js2excel: {
-    excel2json: (files: File[], cb: CallBack, defval?: string) => any;
-    json2excel: (opts: Params) => void;
-    csv2json: (files: File[], cb: CallBack, encode?: string, separator?: string) => any;
+    excel2json: typeof excel2json;
+    json2excel: typeof json2excel;
+    csv2json: typeof csv2json;
 };
 export default js2excel;
